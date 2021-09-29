7324447a213fccbba92f69056e49d3dd460cbdc4
John Doe <oleg.y07.10.03@gmail.com>
2021-09-29 23:16:41 +0300

    commited

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 name = input('Введите имя студента: ')
 secondname = input('Введите фамилию студента: ')
 yearofbirthday = int(input('Введите год рождения студента: '))
-name, secondname = secondname, name
 print(name, secondname, yearofbirthday, sep = "_")
+name, secondname = secondname, name
 print(name, secondname, yearofbirthday+60, sep = "_" )
